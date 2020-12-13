c0b5d938acc8ec1545209a900c79a60da92505fd
José.A Rojo <6jarv91@gmail.com>
2020-12-13 18:33:36 +0100

    Fuck, I was calculating the remainders the wrong way for Chinese Remainder Theorem

--- day-13/data.go
+++ day-13/data.go
@@ -1,7 +1,7 @@
 package main
 
 const exampleData = `939
-17,x,13,19`
+7,13,x,x,59,x,31,19`
 
 const data = `1000186
 17,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,907,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,x,x,x,x,x,x,23,x,x,x,x,x,29,x,653,x,x,x,x,x,x,x,x,x,41,x,x,13`
--- day-13/main.go
+++ day-13/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"log"
+	"math/big"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	// Part One
-	var busIDs []int
-	splitData := strings.Split(data, "\n")
+	var busIDs []int64
+	splitData := strings.Split(exampleData, "\n")
 	startTime, err := strconv.Atoi(splitData[0])
 	if err != nil {
 		panic("start time not a number")
@@ -19,22 +20,24 @@ func main() {
 		if err != nil {
 			continue
 		}
-		busIDs = append(busIDs, busID)
+		busIDs = append(busIDs, int64(busID))
 	}
-	calcPartOne(startTime, busIDs)
+	calcPartOne(int64(startTime), busIDs)
 
 	// PartTwo
-	var remainders []int
+	var remainders []int64
 	for i, rawBusID := range strings.Split(splitData[1], ",") {
-		if rawBusID == "x" {
+		busID, err := strconv.Atoi(rawBusID)
+		if err != nil {
 			continue
 		}
-		remainders = append(remainders, i)
+		remainders = append(remainders, int64((busID-i)%busID)) // Positive modulo of (-i % busID)
 	}
+	log.Println(busIDs, remainders)
 	calcPartTwo(busIDs, remainders)
 }
 
-func calcPartOne(startTime int, busIDs []int) {
+func calcPartOne(startTime int64, busIDs []int64) {
 	for time := startTime; ; time++ {
 		for _, busID := range busIDs {
 			if time%busID == 0 {
@@ -46,52 +49,52 @@ func calcPartOne(startTime int, busIDs []int) {
 	}
 }
 
-func calcPartTwo(busIDs []int, rem []int) {
-	// Start at bus 0, incrementing by busID[0] at every step
-	increment := busIDs[0]
-	accum := increment
-
-	for i := 1; i < len(busIDs); i++ {
-		for (accum+rem[i])%busIDs[i] != 0 {
-			accum += increment
-		}
-		increment *= busIDs[i] // multiply by current busID, so that accum is always correct for its remainder
-	}
-
-	log.Println("Part Two solution:", accum)
-}
-
-/*
-
-//Failed attempt to solve it using CRT:
-
-func calcPartTwo(busIDs []int, rem []int) {
-	log.Println(busIDs)
-	log.Println(rem)
-
-	p := 1 // product of all bus IDs
+func calcPartTwo(busIDs []int64, rem []int64) {
+	p := big.NewInt(1) // product of all bus IDs
 	for _, busID := range busIDs {
-		p *= busID
+		p.Mul(p, big.NewInt(busID))
 	}
 
-	s := 0 // ∑(a*pa*c)
-	for i := 0; i < len(busIDs); i++ {
-		a, b := busIDs[i], rem[i]
-		pa := p / a
-		c := modInv(pa, a)
-		s += b * pa * c
+	s := big.NewInt(0) // ∑(a*pa*c)
+	for i := 1; i < len(busIDs); i++ {
+		b, r := big.NewInt(busIDs[i]), big.NewInt(rem[i])
+
+		pb := big.NewInt(0)
+		pb.Div(p, b)
+
+		inv := modInv(pb, b)
+
+		m := big.NewInt(0)
+		m.Mul(r, pb)
+		m.Mul(m, inv)
+		s.Add(s, m)
 	}
-	log.Println(s, p, s%p)
+
+	// sanity check
+	for i := 0; i < len(busIDs); i++ {
+		b, r := big.NewInt(busIDs[i]), big.NewInt(rem[i])
+		x := big.NewInt(0)
+		if x.Mod(s, b).Cmp(r) != 0 {
+			log.Fatal()
+		}
+		log.Println(s, b, x, r)
+	}
+
+	s.Mod(s, p)
+	log.Println("Part Two solution:", s)
 }
 
 // modulo inverse
-func modInv(a, m int) int {
-	a = a % m
-	for i := 1; i < m; i++ {
-		if (a*i)%m == 1 {
+func modInv(a, m *big.Int) *big.Int {
+	a2 := big.NewInt(0)
+	a2.Mod(a, m)
+	one := big.NewInt(1)
+	for i := big.NewInt(1); i.Cmp(m) < 0; i.Add(i, one) {
+		ai := big.NewInt(0)
+		ai.Mul(a2, i)
+		if ai.Mod(ai, m).Cmp(one) == 0 {
 			return i
 		}
 	}
 	panic("mod inv not found")
 }
-*/
