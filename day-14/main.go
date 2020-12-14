6b385f8b07c1f5f175683d4fbdd4187f044a676f
José.A Rojo <6jarv91@gmail.com>
2020-12-14 23:50:41 +0100

    Day 14: Cleaner!

--- day-14/main.go
+++ day-14/main.go
@@ -8,22 +8,38 @@ import (
 )
 
 func main() {
-	// Part One
+	runPartOne(data)
+	runPartTwo(data)
+}
+
+func mustAtoui(s string) uint64 {
+	i, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+// Part One
+
+func runPartOne(data string) {
 	memory := make(map[uint64]uint64)
 	maskRgx := regexp.MustCompile("^mask = (.+)$")
 	memRgx := regexp.MustCompile("^mem\\[(\\d+)\\] = (\\d+)$")
-	setMask, clrMask := uint64(0), ^uint64(0)
+
+	var setMask, clrMask uint64
 	for _, dataLine := range strings.Split(data, "\n") {
 		maskMatch := maskRgx.FindStringSubmatch(dataLine)
 		if len(maskMatch) != 0 {
 			setMask, clrMask = processMask(maskMatch[1])
 			continue
 		}
+
 		memMatch := memRgx.FindStringSubmatch(dataLine)
+		address := mustAtoui(memMatch[1])
 		value := mustAtoui(memMatch[2])
 		value |= setMask
 		value &= clrMask
-		address := mustAtoui(memMatch[1])
 		memory[address] = value
 	}
 
@@ -32,9 +48,28 @@ func main() {
 		sum += uint64(val)
 	}
 	log.Println("Part One solution:", sum)
+}
 
-	// Part Two
-	memory = make(map[uint64]uint64) // reset memory from part one
+func processMask(s string) (setMask uint64, clrMask uint64) {
+	for i := range s {
+		c := s[len(s)-1-i]
+		switch c {
+		case '1':
+			setMask |= (1 << i)
+		case '0':
+			clrMask |= (1 << i)
+		}
+	}
+	return setMask, ^clrMask
+}
+
+// Part Two
+
+func runPartTwo(data string) {
+	memory := make(map[uint64]uint64) // reset memory from part one
+	maskRgx := regexp.MustCompile("^mask = (.+)$")
+	memRgx := regexp.MustCompile("^mem\\[(\\d+)\\] = (\\d+)$")
+
 	var mask string
 	for _, dataLine := range strings.Split(data, "\n") {
 		maskMatch := maskRgx.FindStringSubmatch(dataLine)
@@ -42,9 +77,11 @@ func main() {
 			mask = maskMatch[1]
 			continue
 		}
+
 		memMatch := memRgx.FindStringSubmatch(dataLine)
 		address := mustAtoui(memMatch[1])
 		addresses := []uint64{0}
+		// calculate all possible addresses by applying the mask
 		// mask[i] is the the most significant bit
 		// mask[35] is be the least significant bit
 		// so we traverse mask from left to right and invert the bit index
@@ -75,40 +112,12 @@ func main() {
 		}
 	}
 
-	sum = 0
+	var sum uint64
 	for _, val := range memory {
 		sum += uint64(val)
 	}
 	log.Println("Part Two solution:", sum)
 }
-
-func mustAtoui(s string) uint64 {
-	i, err := strconv.ParseUint(s, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	return i
-}
-
-// For Part One
-
-func processMask(s string) (uint64, uint64) {
-	var setMask, clrMask uint64
-	for i := range s {
-		c := s[len(s)-1-i]
-		switch c {
-		case 'X':
-			continue
-		case '1':
-			setMask |= (1 << i)
-		case '0':
-			clrMask |= (1 << i)
-		}
-	}
-	return setMask, ^clrMask
-}
-
-// For Part Two
 
 func bitAt(val uint64, index int) uint64 {
 	mask := uint64(1) << index
