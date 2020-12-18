af37e3d2cd64d607e203ec322c066cd1fbb4ee67
José.A Rojo <6jarv91@gmail.com>
2020-12-18 12:39:20 +0100

    Day 17 part two

--- day-17/main.go
+++ day-17/main.go
@@ -7,12 +7,78 @@ import (
 
 func main() {
 	initialState := newGrid(data)
-	// Part One
-	calcPartOne(initialState, 6)
+	log.Println("Part One solution:", len(simulateCycles(initialState, 6).cubes))
+	initialState.useFourthDimension = true
+	log.Println("Part Two solution:", len(simulateCycles(initialState, 6).cubes))
 }
 
+func simulateCycles(initialState grid, cycles int) grid {
+	state := initialState
+	iteration := 0
+	for iteration < cycles {
+		state = state.nextState()
+		iteration++
+	}
+	return state
+}
+
+// Coords type and methods
+
+type coords struct {
+	x, y, z, w int
+}
+
+func (c coords) neighborCoords3D() []coords {
+	neighborCoords := make([]coords, 3*3*3-1)
+	index := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			for k := -1; k <= 1; k++ {
+				if i == 0 && j == 0 && k == 0 {
+					continue
+				}
+				neighborCoords[index] = coords{
+					c.x + i,
+					c.y + j,
+					c.z + k,
+					0,
+				}
+				index++
+			}
+		}
+	}
+	return neighborCoords
+}
+
+func (c coords) neighborCoords4D() []coords {
+	neighborCoords := make([]coords, 3*3*3*3-1)
+	index := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			for k := -1; k <= 1; k++ {
+				for l := -1; l <= 1; l++ {
+					if i == 0 && j == 0 && k == 0 && l == 0 {
+						continue
+					}
+					neighborCoords[index] = coords{
+						c.x + i,
+						c.y + j,
+						c.z + k,
+						c.w + l,
+					}
+					index++
+				}
+			}
+		}
+	}
+	return neighborCoords
+}
+
+// Grid type and methods
+
 type grid struct {
-	cubes map[coords]struct{}
+	cubes              map[coords]struct{}
+	useFourthDimension bool
 }
 
 func newGrid(rawGrid string) grid {
@@ -22,7 +88,7 @@ func newGrid(rawGrid string) grid {
 	for y, dataLine := range strings.Split(rawGrid, "\n") {
 		for x, char := range dataLine {
 			if char == '#' {
-				initialState.cubes[coords{x, y, 0}] = struct{}{}
+				initialState.cubes[coords{x: x, y: y}] = struct{}{}
 			}
 		}
 	}
@@ -31,11 +97,18 @@ func newGrid(rawGrid string) grid {
 
 // relevantCoordsForNextState returns a slice of all cords that need to be checked to create the next state
 func (g grid) relevantCoordsForNextState() []coords {
-	coordsSet := make(map[coords]struct{})
+	coordsSet := make(map[coords]struct{}, len(g.cubes))
 	for coord := range g.cubes {
-		for _, neighbor := range coord.neighborCoords() {
-			coordsSet[neighbor] = struct{}{}
+		if g.useFourthDimension {
+			for _, neighbor := range coord.neighborCoords4D() {
+				coordsSet[neighbor] = struct{}{}
+			}
+		} else {
+			for _, neighbor := range coord.neighborCoords3D() {
+				coordsSet[neighbor] = struct{}{}
+			}
 		}
+
 	}
 	// map keySet to slice
 	coordsSlice := make([]coords, len(coordsSet))
@@ -49,55 +122,27 @@ func (g grid) relevantCoordsForNextState() []coords {
 
 func (g grid) activeNeighbors(c coords) int {
 	var count int
-	for _, neighbor := range c.neighborCoords() {
-		if _, active := g.cubes[neighbor]; active {
-			count++
+	if g.useFourthDimension {
+		for _, neighbor := range c.neighborCoords4D() {
+			if _, active := g.cubes[neighbor]; active {
+				count++
+			}
+		}
+	} else {
+		for _, neighbor := range c.neighborCoords3D() {
+			if _, active := g.cubes[neighbor]; active {
+				count++
+			}
 		}
 	}
 	return count
 }
 
-type coords struct {
-	x, y, z int
-}
-
-func (c coords) neighborCoords() []coords {
-	neighborCoords := make([]coords, 3*3*3-1)
-	index := 0
-	for i := -1; i <= 1; i++ {
-		for j := -1; j <= 1; j++ {
-			for k := -1; k <= 1; k++ {
-				if i == 0 && j == 0 && k == 0 {
-					continue
-				}
-				neighborCoords[index] = coords{
-					c.x + i,
-					c.y + j,
-					c.z + k,
-				}
-				index++
-			}
-		}
-	}
-	return neighborCoords
-}
-
-/******************************************************************/
-/************************* PART ONE STUFF *************************/
-/******************************************************************/
-
-func calcPartOne(initialState grid, cycles int) {
-	state := initialState.nextState()
-	iteration := 1 // we have already done an iteration
-	for iteration < cycles {
-		state = state.nextState()
-		iteration++
-	}
-	log.Println("Solution part one:", len(state.cubes))
-}
-
 func (g grid) nextState() (newState grid) {
-	newState = newGrid("")
+	newState = grid{
+		cubes:              make(map[coords]struct{}),
+		useFourthDimension: g.useFourthDimension,
+	}
 	for _, coord := range g.relevantCoordsForNextState() {
 		_, active := g.cubes[coord]
 		activeNeighbors := g.activeNeighbors(coord)
