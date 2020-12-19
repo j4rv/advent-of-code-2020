21091968b4a3abf6e4d328650d30c5f49c031cd6
José.A Rojo <6jarv91@gmail.com>
2020-12-20 00:33:35 +0100

    Day 19: Added some comments

--- day-19/main.go
+++ day-19/main.go
@@ -40,35 +40,8 @@ func solvePartTwo(rawRules, rawMessages string) {
 	log.Println("Part Two solution:", counter)
 }
 
-func unloopWithDepth(rawRules string, depth int) string {
-	var unlooped string
-	for _, line := range strings.Split(rawRules, "\n") {
-		newLine := line
-		// unloop rule 8
-		if string(line[0:2]) == "8:" {
-			newLine = "8: 42 +"
-		}
-		// unloop rule 11
-		if string(line[0:3]) == "11:" {
-			rule := "42 31"
-			for i := 2; i < depth; i++ {
-				rule += " | "
-				for j := 0; j < i; j++ {
-					rule += " 42 "
-				}
-				for j := 0; j < i; j++ {
-					rule += " 31 "
-				}
-			}
-			newLine = "11: " + rule
-		}
-		unlooped += newLine + "\n"
-	}
-	return unlooped[:len(unlooped)-1]
-}
-
 func parse(rawRules string) string {
-	mem := make(map[int]string)
+	mem := make(map[int]string) // rule index to that rule's regexp
 	rules := strings.Split(rawRules, "\n")
 	for {
 		if _, ok := mem[0]; ok {
@@ -79,11 +52,11 @@ func parse(rawRules string) string {
 			tokens := strings.Fields(rule) // ignore first token, it's the rule index
 			var i int
 			fmt.Sscanf(tokens[0], "%d:", &i)
-			tokens = tokens[1:]
 			// visited check:
 			if _, ok := mem[i]; ok {
 				continue
 			}
+			tokens = tokens[1:] // remove the index from the tokens
 			// base case:
 			if len(tokens) == 1 && tokens[0][0] == '"' {
 				mem[i] = string(tokens[0][1])
@@ -112,7 +85,37 @@ func parse(rawRules string) string {
 			mem[i] = ruleRgx
 		}
 	}
+	// we are interested in an exact match of rule 0
 	return "^" + mem[0] + "$"
+}
+
+// for part two:
+// generates x possible loop variations (loop depth) for rules with loops (8 and 11)
+func unloopWithDepth(rawRules string, depth int) string {
+	var unlooped string
+	for _, line := range strings.Split(rawRules, "\n") {
+		newLine := line
+		// unloop rule 8
+		if string(line[0:2]) == "8:" {
+			newLine = "8: 42 +"
+		}
+		// unloop rule 11
+		if string(line[0:3]) == "11:" {
+			rule := "42 31"
+			for i := 2; i < depth; i++ {
+				rule += " | "
+				for j := 0; j < i; j++ {
+					rule += " 42 "
+				}
+				for j := 0; j < i; j++ {
+					rule += " 31 "
+				}
+			}
+			newLine = "11: " + rule
+		}
+		unlooped += newLine + "\n"
+	}
+	return unlooped[:len(unlooped)-1]
 }
 
 // I should make an aocutils package...
